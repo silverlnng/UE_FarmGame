43fc96e3223d734e605241a9a076436ccdce724f
yjkwak <silverlnng0721@gmail.com>
2023-11-23 20:23:52 +0900

    3단계성장시 캐릭터와 overlap되면 원래대로 돌아가고 다시 타이머 시작

--- Source/FarmProject/Private/Item/Crop.cpp
+++ Source/FarmProject/Private/Item/Crop.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Item/Crop.h"
+#include "MainFarmCharacter.h"
 
 ACrop::ACrop()
 {
@@ -31,8 +32,20 @@ void ACrop::Tick(float DeltaSeconds)
 void ACrop::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
 	//super 으로 부모의 OnSphereOverlap 함수 사용 + 동시에 override으로 재정의
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	if(	ItemMesh_2->IsVisible())
+	{
+		AMainFarmCharacter* mainFarmCharacter = Cast<AMainFarmCharacter>(OtherActor);
+		if(mainFarmCharacter)
+		{
+			GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Yellow, "crop");
+			//오직한번만 발생하도록
+			ItemMesh_2->ToggleVisibility();
+			ItemMesh->ToggleVisibility();
+			GetWorldTimerManager().SetTimer(growingTimer,this,&ACrop::RepeatingGrowingFunction,growingRate,true,firstDelay);
+		}
+	}
 	
 }
 
@@ -61,11 +74,13 @@ void ACrop::RepeatingGrowingFunction()
 		temp_plus->ToggleVisibility();
 	}
 	
-	UE_LOG(LogTemp, Log, TEXT("RepeatingCalls :: &d"), RepeatingCalls);
+	GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Black,  FString::FromInt(RepeatingCalls));
+	
 	if (RepeatingCalls>=RepeatingCallsMax)
 	{
 		GetWorldTimerManager().ClearTimer(growingTimer);	// growingTimer 은 이제 다른 타이머에 재사용 가능합니다.
 		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Black, "timerEnd");
+		RepeatingCalls=0;
 	}
 }
 
