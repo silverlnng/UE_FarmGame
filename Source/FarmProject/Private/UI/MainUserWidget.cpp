6814cd1a459c6ecb1952493fd96cfa9c9ea3cb78
yjkwak <silverlnng0721@gmail.com>
2023-11-23 14:21:12 +0900

    DAY Hour Min 위젯 띄우기 성공

--- Source/FarmProject/Private/UI/MainUserWidget.cpp
+++ Source/FarmProject/Private/UI/MainUserWidget.cpp
@@ -9,9 +9,30 @@ void UMainUserWidget::SetClockText(float num)
 	if(clockText)
 	{
 		int32 IntNumb = FMath::FloorToInt(num);
+		IntNumb=IntNumb*24;
 		
-		FString IntAsString = FString::FromInt(IntNumb);
-		FText ftext = FText::FromString(IntAsString);
-		clockText->SetText(ftext);
+		int32 day = IntNumb / (24 * 3600);
+		//FString dayString = FString::FromInt(day);
+		//FText daytext = FText::FromString(dayString);
+		
+		IntNumb = IntNumb % (24 * 3600); 
+		int32 hour = IntNumb / 3600; 
+		//FString hourString = FString::FromInt(hour);
+		//FText hourtext = FText::FromString(hourString);
+		
+		IntNumb %= 3600; 
+		int32 minutes = IntNumb / 60 ;
+		//FString minuteString = FString::FromInt(minutes);
+		//FText minutetext = FText::FromString(minuteString);
+		
+		//FString IntAsString = FString::FromInt(IntNumb);
+
+		//FString timerString = FString::Printf(TEXT("[%s] ÀÏ,[%s] :,[%s]"),*dayString ,*hourString ,*minuteString);
+		
+		FText timerText = FText::Format(FText::FromString("{0}DAY  {1}Hour  {2}Min"),day ,hour ,minutes);
+		
+		//FText ftext = FText::FromString(timerText);
+		
+		clockText->SetText(timerText);
 	}
 }
