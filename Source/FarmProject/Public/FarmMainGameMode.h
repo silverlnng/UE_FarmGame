1f5711daaf2e1dc1bcd8c15b8a2ff89eaec0564f
yjkwak <silverlnng0721@gmail.com>
2023-11-25 17:52:04 +0900

    image 추가

--- Source/FarmProject/Public/FarmMainGameMode.h
+++ Source/FarmProject/Public/FarmMainGameMode.h
@@ -28,6 +28,7 @@ public:
 	TSubclassOf<class AFarmHUD> FarmHUD_BP;
 	AFarmHUD* FarmHUD;
 	
+	////시간설정
 	UPROPERTY(EditAnywhere, Category = "Time")
 	float currentTime = 1800;
 	//처음시작을 오후12시 부터 시작하기 60*60*12 /24 = 1800
@@ -35,4 +36,8 @@ public:
 	int32 initTime = 0;
 	UPROPERTY(EditAnywhere, Category = "Time")
 	ULightComponent* directionalLight;	//1시간에 y축 15도 씩 회전
+	////
+	
+	
+	///
 };
